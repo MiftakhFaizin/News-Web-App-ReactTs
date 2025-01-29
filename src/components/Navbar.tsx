349a1148c764dda35ce214a2415fa38d2177b098
MiftakhFaizin <miftakhdamar07@gmail.com>
2025-01-29 20:28:21 +0700

    minor changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,25 +12,29 @@ const Navbar = () => {
             <div className="container bg-white/70 blur-md flex justify-between items-center py-5 px-10 gap-8">
                 <h1 className="font-bold text-3xl max-md:hidden"><Link to="">News</Link></h1>
                 <RxHamburgerMenu onClick={() => setIsHamburgerMenu(true)} className="md:hidden" />
-                <ul onClick={() => {setIsHamburgerMenu(false)}} className={`justify-self-center flex gap-5 md:overflow-x-auto md:py-3 max-md:bg-white max-md:fixed max-md:pt-10 max-md:gap-10 max-md:top-0 max-md:left-0 max-md:min-h-screen max-md:w-[80%] max-md:flex-col max-md:z-10 max-md:shadow-2xl max-md:shadow-slate-600 ${isHamburgermenu ? "" : "max-md:-translate-x-[100%]"} transition-all duration-200 linear`}>
+                <ul className={`justify-self-center flex gap-5 md:overflow-x-auto md:py-3 max-md:bg-white max-md:fixed max-md:pt-10 max-md:gap-10 max-md:top-0 max-md:left-0 max-md:min-h-screen max-md:w-[80%] max-md:flex-col max-md:z-10 max-md:shadow-2xl max-md:shadow-slate-600 ${isHamburgermenu ? "" : "max-md:-translate-x-[100%]"} transition-all duration-200 linear`}>
                     <h1 className="font-bold text-3xl md:hidden self-center"><Link to="">News</Link></h1>
                     <IoClose onClick={() => setIsHamburgerMenu(false)} className="md:hidden ml-5" />
                     <NavLink
+                        onClick={() => {setIsHamburgerMenu(false)}}
                         className={({ isActive }) => (isActive ? "self-start bg-black text-white rounded-md max-md:ml-5 px-5 py-1" : "self-start md:border border-gray-700 rounded-md px-5 py-1")}
                         to="">
                         Home
                     </NavLink>
                     <NavLink
+                        onClick={() => {setIsHamburgerMenu(false)}}
                         className={({ isActive }) => (isActive ? "self-start bg-black text-white rounded-md max-md:ml-5 px-5 py-1" : "self-start md:border border-gray-700 rounded-md px-5 py-1")}
                         to="category-page/Booming">
                         Booming
                     </NavLink>
                     <NavLink 
+                        onClick={() => {setIsHamburgerMenu(false)}}
                         className={({ isActive }) => (isActive ? "self-start bg-black text-white rounded-md max-md:ml-5 px-5 py-1" : "self-start md:border border-gray-700 rounded-md px-5 py-1")}
                         to="category-page/Sports">
                         Sports
                     </NavLink>
                     <NavLink 
+                        onClick={() => {setIsHamburgerMenu(false)}}
                         className={({ isActive }) => (isActive ? "self-start bg-black text-white rounded-md max-md:ml-5 px-5 py-1" : "self-start md:border border-gray-700 rounded-md px-5 py-1")}
                         to="category-page/Technology">
                         Technology
