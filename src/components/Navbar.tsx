f85c40fa2c35ae95f3bb3fde320e737905957c82
MiftakhFaizin <miftakhdamar07@gmail.com>
2025-01-29 13:55:49 +0700

    fix navbar overflow

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
             <div className="container flex justify-between items-center py-5 px-10 gap-8">
                 <h1 className="font-bold text-3xl max-md:hidden"><Link to="">News</Link></h1>
                 <RxHamburgerMenu onClick={() => setIsHamburgerMenu(true)} className="md:hidden" />
-                <ul onClick={() => {setIsHamburgerMenu(false)}} className={`justify-self-center flex gap-5 md:overflow-x-auto md:px-2 max-md:bg-white max-md:fixed max-md:pt-10 max-md:gap-10 max-md:top-0 max-md:left-0 max-md:min-h-screen max-md:w-[80%] max-md:flex-col max-md:z-10 max-md:shadow-2xl max-md:shadow-slate-600 ${isHamburgermenu ? "" : "max-md:-translate-x-[100%]"} transition-all duration-200 linear`}>
+                <ul onClick={() => {setIsHamburgerMenu(false)}} className={`justify-self-center flex gap-5 md:overflow-x-auto md:py-3 max-md:bg-white max-md:fixed max-md:pt-10 max-md:gap-10 max-md:top-0 max-md:left-0 max-md:min-h-screen max-md:w-[80%] max-md:flex-col max-md:z-10 max-md:shadow-2xl max-md:shadow-slate-600 ${isHamburgermenu ? "" : "max-md:-translate-x-[100%]"} transition-all duration-200 linear`}>
                     <h1 className="font-bold text-3xl md:hidden self-center"><Link to="">News</Link></h1>
                     <IoClose onClick={() => setIsHamburgerMenu(false)} className="md:hidden ml-5" />
                     <NavLink
